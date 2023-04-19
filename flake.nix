4a2d00745bd5ba2e48685515782aa69359043e6a
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-04-19 10:33:56 +0300

    Revert "nixpkgs: Update to commit with sudo fix merged"
    This reverts commit 4d8611142d2e8c694c07962f5cacfaa7d25c7169.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=90e664602857676615322b8fb5ddbed607b77a82";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=6a9788519f76fd732224cc7e4e64425b4be966f";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
